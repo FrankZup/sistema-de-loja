af3a6025fcccb19dd02ea677c9e8aec97a0f3535
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-24 20:56:25 -0300

    Criar modelo para Item do Pedido

---
+++ ItemPedido.java
@@ -0,0 +1,32 @@
+package mentoria.projeto;
+
+public class ItemPedido {
+    private Produto produto;
+    private int quantidade;
+    private double valorProduto;
+
+    public ItemPedido() {}
+
+    public ItemPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() { return produto; }
+    public void setProduto(Produto produto) { this.produto = produto; }
+
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
+
+    public double getValorProduto() { return valorProduto; }
+    public void setValorProduto(double valorProduto) { this.valorProduto = valorProduto; }
+
+    @Override
+    public String toString() {
+        return "ItemPedido{" +
+                "produto=" + produto +
+                ", quantidade=" + quantidade +
+                ", valorProduto=" + valorProduto +
+                '}';
+    }
+}
