8ce383f4c698807b7b2b9f9d68574b56b9b56c8f
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-24 20:43:08 -0300

    Criar modelo de Pessoa

---
+++ Pessoa.java
@@ -0,0 +1,22 @@
+package mentoria.projeto;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+
+    public Pessoa() {}
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
+}
