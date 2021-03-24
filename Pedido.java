6f0f730e7183902305c15635aeb44fe8e66f2902
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-24 20:58:14 -0300

    Criar modelo de Pedido

---
+++ Pedido.java
@@ -0,0 +1,50 @@
+package mentoria.projeto;
+
+import mentoria.projeto.enums.Status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+    private int id;
+    private Cliente cliente;
+    private String observacao;
+    private Status status;
+    private List<ItemPedido> itemPedido = new ArrayList<>();
+
+    public Pedido() {}
+
+    public Pedido(int id, Cliente cliente, String observacao, Status status) {
+        this.id = id;
+        this.cliente = cliente;
+        this.observacao = observacao;
+        this.status = status;
+        this.itemPedido = itemPedido;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public Cliente getCliente() { return cliente; }
+    public void setCliente(Cliente cliente) { this.cliente = cliente; }
+
+    public String getObservacao() { return observacao; }
+    public void setObservacao(String observacao) { this.observacao = observacao; }
+
+    public Status getStatus() { return status; }
+    public void setStatus(Status status) { this.status = status; }
+
+    public List<ItemPedido> getItemPedido() { return itemPedido; }
+    public void setItemPedido(List<ItemPedido> itemPedido) { this.itemPedido = itemPedido; }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "id=" + id +
+                ", cliente=" + cliente +
+                ", observacao='" + observacao + '\'' +
+                ", status=" + status +
+                ", itemPedido=" + itemPedido +
+                '}';
+    }
+}
