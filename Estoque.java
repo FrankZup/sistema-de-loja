ef7049b41f6e85e0ee07b0aaa08a250efb2d63cb
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-03-24 21:12:10 -0300

    Criar método para verificar o saldo atual do produto

--- Estoque.java
+++ Estoque.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Estoque {
 
-    private HashMap<Produto, Integer> produtosEstoque = new HashMap<>();
+    private static HashMap<Produto, Integer> produtosEstoque = new HashMap<>();
 
     public void adicionarProdutoEstoque(Produto produto) {
         produtosEstoque.put(produto, 0);
@@ -12,5 +12,9 @@ public class Estoque {
 
     public void removerProdutoEstoque(Produto produto){
         produtosEstoque.remove(produto);
+    }
+
+    public static int verificarSaldoAtualProduto(Produto produto){
+        return produtosEstoque.get(produto).intValue();
     }
 }
