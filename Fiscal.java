6691c1ab72f5cdb55c9cab644f56bd8d8cc3a579
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-04-08 11:56:36 -0300

    Criado as classes Nota Fiscal e Fiscal

---
+++ Fiscal.java
@@ -0,0 +1,10 @@
+package mentoria.projeto;
+
+public class Fiscal extends Empresa {
+
+
+    public NotaFiscal emitirNotaFiscal(Pedido pedido){
+        NotaFiscal notaFiscal = null;
+        return null;
+    }
+}
---
+++ NotaFiscal.java
@@ -0,0 +1,9 @@
+package mentoria.projeto;
+
+public class NotaFiscal {
+    private int numeroNota;
+    private Empresa empresa;
+    private Cliente cliente;
+    private double valorNota;
+
+}
